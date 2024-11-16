5d569b0504d612fc0cee64a5f119bd2b5bcdf689
Oksana Svishcho <your.email@example.com>
2024-11-16 17:58:22 +0200

    Оновлено структуру db/migration та додано валідацію ID для планет

--- src/main/java/org/example/Planet.java
+++ src/main/java/org/example/Planet.java
@@ -1,26 +1,28 @@
 package org.example;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "planet")
 public class Planet {
+
     @Id
-    @Column(name = "id", nullable = false, length = 10)
+    @Column(name = "id", nullable = false, unique = true, length = 10)
+    @Pattern(regexp = "^[A-Z0-9]+$", message = "ID повинен складатися виключно з латинських букв у верхньому регістрі та цифр.")
     private String id;
 
     @Column(name = "name", nullable = false, length = 500)
     private String name;
 
-    public Planet(String id) {
-        this.id = id;
+    public Planet() {
     }
 
-    public Planet() {
-
+    public Planet(String id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public String getId() {
@@ -38,6 +40,6 @@ public class Planet {
     public void setName(String name) {
         this.name = name;
     }
-
 }
+
 
